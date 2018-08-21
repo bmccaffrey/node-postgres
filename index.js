6471978f778976b680d6f65af67309899b225a8d
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-21 16:53:00 -0500

    import bodyParser

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Imports
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const { Client } = require('pg');
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 const client = new Client({
   user: 'generic',
