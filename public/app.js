03739415af2f35959459838e3cd59be1e3678422
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-21 23:59:33 -0500

    allow for multiple recipes to be displayed

--- public/app.js
+++ public/app.js
@@ -1,9 +1,14 @@
-var recipeName = document.querySelector('h1');
-var ingredients = document.querySelector('h2');
-var directions = document.querySelector('h3');
+var body = document.querySelector('body');
+// var recipeName = document.querySelector('h1');
+var root = document.getElementById('root');
+// var ingredients = document.querySelector('h2');
+// var directions = document.querySelector('h3');
 var selector = document.querySelector('select');
 var button = document.querySelector('button');
 function get() {
+  while (root.childElementCount > 0) {
+    root.lastElementChild.remove();
+  }
   let value = selector.value;
   console.log(value);
   let recipeNames = fetch(`/recipes?recipes=${value}`)
@@ -11,10 +16,18 @@ function get() {
       return response.json();
     })
     .then(function(response) {
-      console.log(response[0]);
-      recipeName.textContent = response[0].name;
-      ingredients.textContent = response[0].ingredients;
-      directions.textContent = response[0].directions;
+      console.log(value);
+      console.log(response);
+      response.forEach(element => {
+        console.log(Object.values(element));
+        let x = document.createElement('h1');
+        // x.textContent = Object.values(element)[0];
+        x.textContent = Object.values(element).slice(0);
+        root.append(x);
+      });
+      // recipeName.textContent = response[0].name;
+      // ingredients.textContent = response[0].ingredients;
+      // directions.textContent = response[0].directions;
     })
     .catch(error => console.error(error));
 }
