74a9027fd08653ff1e289662708d5088211ff8b0
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-22 00:00:29 -0500

    remove previous variable declarations

--- public/app.js
+++ public/app.js
@@ -1,8 +1,5 @@
 var body = document.querySelector('body');
-// var recipeName = document.querySelector('h1');
 var root = document.getElementById('root');
-// var ingredients = document.querySelector('h2');
-// var directions = document.querySelector('h3');
 var selector = document.querySelector('select');
 var button = document.querySelector('button');
 function get() {
@@ -21,13 +18,9 @@ function get() {
       response.forEach(element => {
         console.log(Object.values(element));
         let x = document.createElement('h1');
-        // x.textContent = Object.values(element)[0];
         x.textContent = Object.values(element).slice(0);
         root.append(x);
       });
-      // recipeName.textContent = response[0].name;
-      // ingredients.textContent = response[0].ingredients;
-      // directions.textContent = response[0].directions;
     })
     .catch(error => console.error(error));
 }
